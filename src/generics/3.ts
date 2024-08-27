356dea79a9bbe8f3f124b3e3ed1c0ce69f7d6433
BilousPM <bilouspm@gmail.com>
2024-08-27 09:24:15 -0600

    adds empty object

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,4 +1,3 @@
 function merge<T extends object, Y extends object>(objA: T, objB: Y): object {
-  return Object.assign(objA, objB);
+  return Object.assign({}, objA, objB);
 }
-console.log(merge({ fred: "rr" }, {}));
